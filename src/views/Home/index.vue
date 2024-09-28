a11541fad5ff0eea1a066dc163f305b69f55fb7e
lijiangtao <3530812934@qq.com>
2024-09-28 12:33:09 +0800

    轮播图演示

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { getHomePageDataAPI } from '@/api/home'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 const homePageData = ref({})
 // 获取首页数据
 const getHomePageData = async () => {
@@ -10,8 +10,21 @@ const getHomePageData = async () => {
 }
 // 调用 获取首页数据方法
 getHomePageData()
+
+// 首页轮播图数据
+const bannerList = computed(
+  () => homePageData.value.blocks?.[0].extInfo.banners
+)
 </script>
 
 <template>
-  <div class="home">home</div>
+  <div class="home">
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <van-swipe-item v-for="item in bannerList" :key="item">
+        <van-image width="100%" height="100%" :src="item.pic" />
+      </van-swipe-item>
+    </van-swipe>
+  </div>
 </template>
+
+<style scoped></style>
