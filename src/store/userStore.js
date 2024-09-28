fe1048a3b0bc8697003f885cfab6ae4da9ddc248
lijiangtao <3530812934@qq.com>
2024-09-28 12:52:42 +0800

    组合式store

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,29 +1,14 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/prefer-default-export */
-import { defineStore } from "pinia";
-import to from "await-to-js";
-import { loginByPhone } from "@/api";
-import router from "@/router";
+import { defineStore } from 'pinia'
 
-export const useUserStore = defineStore("user", {
-  state() {
-    return {
-      userInfo: null,
-    };
+export const useUserStore = defineStore(
+  'user',
+  () => {
+    const userInfo = ref({})
+    return { userInfo }
   },
-  getters: {
-    cookie(state) {
-      return state.userInfo?.cookie;
-    },
-  },
-  actions: {
-    async userLogin(data) {
-      const [err, res] = await to(loginByPhone(data));
-      if (res) {
-        this.userInfo = res.data;
-        // router.replace("/");
-      }
-    },
-  },
-  persist: true,
-});
+  {
+    persist: true
+  }
+)
