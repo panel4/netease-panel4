9127407f1894d936a6f5e298c03fd843bca3896d
lijiangtao <3530812934@qq.com>
2024-09-28 12:21:33 +0800

    获取首页数据演示

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,13 +1,14 @@
 <script setup>
 import { getHomePageDataAPI } from '@/api/home'
 import { ref } from 'vue'
-const homePageData = ref([])
+const homePageData = ref({})
+// 获取首页数据
 const getHomePageData = async () => {
   const res = await getHomePageDataAPI()
   homePageData.value = res.data
   console.log(res.data)
 }
-// 调用
+// 调用 获取首页数据方法
 getHomePageData()
 </script>
 
